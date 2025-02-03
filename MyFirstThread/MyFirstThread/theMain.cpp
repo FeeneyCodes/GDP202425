6695e2501d64aa5eff5a95584b3d28a76c8af174
michaelfeeney <michaelwcfeeney@gmail.com>
2025-02-03 12:42:55 -0500

    My 2nd thread!

--- MyFirstThread/MyFirstThread/theMain.cpp
+++ MyFirstThread/MyFirstThread/theMain.cpp
@@ -23,24 +23,59 @@ float getAverage(std::vector< float > theNums)
 	return theTotal;
 }
 
+// Global variables
+std::vector< float > g_theNums;
+float g_average = 0;
+
+
 // Function we call has to be in a specific format:
 // https://learn.microsoft.com/en-us/previous-versions/windows/desktop/legacy/ms686736(v=vs.85)
-DWORD WINAPI getAverageThreadProc(LPVOID lpParameter)
+DWORD WINAPI getAverageThreadProc_V1(LPVOID lpParameter)
 {
 	std::cout << "Hello from the thread!" << std::endl;
-	// TODO: Amazing code here
+
+	::g_average = getAverage(g_theNums);
+	 
 	return 0;
 }
 
-int main(int argc, char* argv[])
+
+struct sGetAverageParams
 {
 	std::vector< float > theNums;
+	float average = 0.0f;
+};
+
+DWORD WINAPI getAverageThreadProc_V2(LPVOID params)
+{
+	// void* pVoidParams = (void*)&myData;
+
+	sGetAverageParams* pMyData = (sGetAverageParams*)params;
+
+	float theTotal = 0.0f;
+	for (float theNum : pMyData->theNums)
+	{
+		theTotal += theNum;
+	}
+
+	pMyData->average = theTotal / (float)(pMyData->theNums.size());
+
+	return 0;
+}
+
+
+int main(int argc, char* argv[])
+{
+//	std::vector< float > theNums;
+	sGetAverageParams myData;
 
 	std::cout << "Adding numbers to vector...";
 
 	for (unsigned int count = 0; count != 10'000'000; count++)
 	{
-		theNums.push_back(g_getRandomFloat(-1000.0f, 1000.0f));
+//		theNums.push_back(g_getRandomFloat(-1000.0f, 1000.0f));
+//		::g_theNums.push_back(g_getRandomFloat(-1000.0f, 1000.0f));
+		myData.theNums.push_back(g_getRandomFloat(-1000.0f, 1000.0f));
 	}
 
 	std::cout << "done." << std::endl;
@@ -48,24 +83,33 @@ int main(int argc, char* argv[])
 	std::cout << "Getting average...";
 
 //	float average = getAverage(theNums);
-	float average = 0.0f;
+//	float average = 0.0f;
 
 	DWORD threadId = 0;
+
+	void* pVoidParams = (void*)&myData;
 
 	HANDLE hThread = CreateThread(
 		NULL,	// If lpThreadAttributes is NULL, the thread gets a default security descriptor.
 		0,		// If this parameter is zero, the new thread uses the default size
 //		getAverage, <-- Not allowed. Must be a certain signature
-		getAverageThreadProc, // pointer to the application-defined function to be executed by the thread.
-		NULL,	// LPVOID lpParameter,
+		getAverageThreadProc_V2, // pointer to the application-defined function to be executed by the thread.
+		pVoidParams,	// LPVOID lpParameter,
 		0,		// 0 = start right away (or CREATE_SUSPENDED) 
 		&threadId);		// LPDWORD lpThreadId
 
 	// Start up the thread now...
 //	ResumeThread(hThread);		// if we CreateThread() as CREATE_SUSPENDED
 
+	// Wait for some ms...
+	std::cout << "Sleeping..." << std::endl;
+	Sleep(1000);
+	std::cout << "I'm awake!" << std::endl;
+
 	std::cout << "done." << std::endl;
-	std::cout << "The average: " << average << std::endl;
+//	std::cout << "The average: " << average << std::endl;
+//	std::cout << "The average: " << ::g_average << std::endl;
+	std::cout << "The average: " << myData.average << std::endl;
 
 
 	return 0;

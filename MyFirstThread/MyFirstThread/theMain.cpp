f1f704f164c905959e348a360e87b806c8fc114e
michaelfeeney <michaelwcfeeney@gmail.com>
2025-02-03 12:22:17 -0500

    My 1st real thread!

--- MyFirstThread/MyFirstThread/theMain.cpp
+++ MyFirstThread/MyFirstThread/theMain.cpp
@@ -1,5 +1,7 @@
 #include <vector>
 #include <iostream>
+#include <Windows.h>	// for CreateThread
+#include <process.h>	// for _beginthreadex
 
 // https://stackoverflow.com/questions/5289613/generate-random-float-between-two-floats
 float g_getRandomFloat(float a, float b) {
@@ -21,12 +23,20 @@ float getAverage(std::vector< float > theNums)
 	return theTotal;
 }
 
+// Function we call has to be in a specific format:
+// https://learn.microsoft.com/en-us/previous-versions/windows/desktop/legacy/ms686736(v=vs.85)
+DWORD WINAPI getAverageThreadProc(LPVOID lpParameter)
+{
+	std::cout << "Hello from the thread!" << std::endl;
+	// TODO: Amazing code here
+	return 0;
+}
 
 int main(int argc, char* argv[])
 {
 	std::vector< float > theNums;
 
-	std::cout << "Adding numbers...";
+	std::cout << "Adding numbers to vector...";
 
 	for (unsigned int count = 0; count != 10'000'000; count++)
 	{
@@ -35,9 +45,26 @@ int main(int argc, char* argv[])
 
 	std::cout << "done." << std::endl;
 
+	std::cout << "Getting average...";
 
-	float average = getAverage(theNums);
+//	float average = getAverage(theNums);
+	float average = 0.0f;
 
+	DWORD threadId = 0;
+
+	HANDLE hThread = CreateThread(
+		NULL,	// If lpThreadAttributes is NULL, the thread gets a default security descriptor.
+		0,		// If this parameter is zero, the new thread uses the default size
+//		getAverage, <-- Not allowed. Must be a certain signature
+		getAverageThreadProc, // pointer to the application-defined function to be executed by the thread.
+		NULL,	// LPVOID lpParameter,
+		0,		// 0 = start right away (or CREATE_SUSPENDED) 
+		&threadId);		// LPDWORD lpThreadId
+
+	// Start up the thread now...
+//	ResumeThread(hThread);		// if we CreateThread() as CREATE_SUSPENDED
+
+	std::cout << "done." << std::endl;
 	std::cout << "The average: " << average << std::endl;
 
 

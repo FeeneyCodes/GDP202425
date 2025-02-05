d096549e94e8849aaa15601bef3c96e0f65bcfd4
michaelfeeney <michaelwcfeeney@gmail.com>
2025-02-05 15:35:23 -0500

    TryCriticalSection

--- MyFirstThread/MyFirstThread/theMain_IO_ThreadSafe.cpp
+++ MyFirstThread/MyFirstThread/theMain_IO_ThreadSafe.cpp
@@ -3,7 +3,18 @@
 // Gets rid of most of the bloated windows stuff
 #define WIN32_LEAN_AND_MEAN
 
+
+// We want to protect this data.
+// Only ONE thread at a time is using this data
 int g_totalWaitTime = 0;
+CRITICAL_SECTION g_CS_TotalWaitTime;
+
+
+struct sData
+{
+	int ID;
+	int totalTime = 0;
+};
 
 
 DWORD WINAPI threadProc_01(LPVOID params)
@@ -11,18 +22,37 @@ DWORD WINAPI threadProc_01(LPVOID params)
 	// int id		<-- what it is
 	// (void*)&id	<-- what I passed
 
-	int* p_id = (int*)params;
-	int id = *p_id;		// Deref (make copy)
+	sData* pData = (sData*)params;
 
 	for (int count = 0; count != 100; count++)
 	{
 		int waitTime = (rand() % 100) + 50;
 
-		::g_totalWaitTime += waitTime;
+//		std::cout << waitTime << std::endl;
+
+		// Global total 
+		//
+		//EnterCriticalSection(&g_CS_TotalWaitTime);
+		// 
+		// Note: Returns type "BOOL" *not* type "bool" 
+		// (BOOL is really an int)
+		if (TryEnterCriticalSection(&g_CS_TotalWaitTime) == TRUE)
+		{
+			::g_totalWaitTime += waitTime;
+
+			LeaveCriticalSection(&g_CS_TotalWaitTime);
+		}
+		//
+		//
+
+		// it's own total
+		pData->totalTime += waitTime;
 
 		Sleep(waitTime);
 		
-		std::cout << "Hey! (from " << id << ")" << std::endl;
+//		EnterCriticalSection(&g_CS_TotalWaitTime);
+		std::cout << "Hey! (from " << pData->ID << ")" << std::endl;
+//		LeaveCriticalSection(&g_CS_TotalWaitTime);
 	}
 	return 0;
 }
@@ -30,26 +60,45 @@ DWORD WINAPI threadProc_01(LPVOID params)
 
 int main()
 {
-	
+
 	HANDLE hThread[10];
 	DWORD ThreadId[10];
-	int threadNumber[10];
+	sData paramData[10];
 
-	for (int id = 0; id < 10; id++)
+	// Set this up
+	InitializeCriticalSection(&g_CS_TotalWaitTime);
+
+	for (int id = 0; id < 1000; id++)
 	{
-		threadNumber[id] = id;
+		paramData[id].ID = id;
+		paramData[id].totalTime = 0;
 
 		hThread[id] = CreateThread(
 			NULL,				// security
 			0,					// stack size
 			threadProc_01,		// procedure
-			(void*)&(threadNumber[id]),					// LPVOID lpParameter,
+			(void*)&(paramData[id]),					// LPVOID lpParameter,
 			0,					// 0, CREATE_SUSPENDED 
-			&(ThreadId[id]) );	// LPDWORD lpThreadId
+			&(ThreadId[id]));	// LPDWORD lpThreadId
 	}
 
-	Sleep(5000);
+	//	WaitForSingleObject(hThread[0], INFINITE);
+	WaitForMultipleObjects(
+		10,
+		hThread,
+		TRUE,
+		5000);
+//	MAXIMUM_WAIT_OBJECTS
 
+	//Sleep(5000);
+
+	//int localTotalTime = 0;
+	//for (int id = 0; id < 10; id++)
+	//{
+	//	localTotalTime += paramData[id].totalTime;
+	//}
+
+	DeleteCriticalSection(&g_CS_TotalWaitTime);
 
 	return 0;
 }

9fad8a76cb0c1ce5262ac066fcc2586215c8a5c0
michaelfeeney <michaelwcfeeney@gmail.com>
2025-03-04 16:13:58 -0500

    Added basic methods to the wraper

--- NoodleNexus/NoodleNexus/theMain.cpp
+++ NoodleNexus/NoodleNexus/theMain.cpp
@@ -204,7 +204,6 @@ int main(void)
     AABBOctTree();
 
 
-    fncPhysXWraper("Hello!");
 
 
 
--- NoodleNexus/cPhysXWraper/cPhysXWraper.cpp
+++ NoodleNexus/cPhysXWraper/cPhysXWraper.cpp
@@ -2,13 +2,82 @@
 //
 #include "cPhysXWraper.h"
 
-#include <iostream>
+// For PIMPL pattern, this include is ONLY in the CPP file
+// (not in the header)
+//#include "cPhysXWrap_Imp.h"
 
-// TODO: This is an example of a library function
-void fncPhysXWraper(std::string theMessage)
+cPhysXWraper::cPhysXWraper()
 {
-	std::cout << "Hey, from the library: "
-		<< theMessage << std::endl;
+//	this->m_pImp = new cPhysXWrap_Imp();
+}
+
+cPhysXWraper::~cPhysXWraper()
+{
+//	delete this->m_pImp;
+}
+
+// HACK: Remove this later
+//void cPhysXWraper::HACK_ShootBall(void)
+//{
+//	this->m_pImp->HACK_ShootBall();
+//	return;
+//}
+
+void cPhysXWraper::getSceneActors(std::vector<cPhysicsObjectTypes>& vecPhysActors)
+{
+//	this->m_pImp->getSceneActors(vecPhysActors);
+	return;
+}
+
+
+
+void cPhysXWraper::initPhysics(bool interactive, std::string PVD_Host_Address /*= "127.0.0.1"*/)
+{
+//	this->m_pImp->initPhysics(interactive, PVD_Host_Address);
+
 
 	return;
 }
+
+void cPhysXWraper::update(void)
+{
+//	this->m_pImp->update();
+	return;
+}
+
+void cPhysXWraper::cleanupPhysics(bool interactive)
+{
+//	this->m_pImp->cleanupPhysics(interactive);
+
+	return;
+}
+
+void cPhysXWraper::AddBox(
+	std::string name,
+	glm::vec3 halfExtents,
+	glm::vec3 position,
+	glm::vec4 EulerOrientation, float mass)
+{
+
+
+
+	return;
+}
+
+void cPhysXWraper::AddSphere(
+	std::string name,
+	float radius,
+	glm::vec3 position,
+	glm::vec4 EulerOrientation,
+	float mass)
+{
+
+
+
+
+
+	return;
+}
+
+
+
